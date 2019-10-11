ed2de13f7e6b3d62a314ba0b85e790d4045e2207
javezbak <ja2757@nyu.edu>
2019-10-11 13:36:05 -0400

    Intiial implementation from Callan's example and Ward's paper

--- AsBinaryString/compute.hs
+++ AsBinaryString/compute.hs
@@ -1,15 +1,20 @@
 import Data.Bits
 
+-- A = 1, B = 0
 data DNA = A | B deriving (Show, Eq)
 
--- A = 1, B = 0
--- original values: "1100111", "1010101", "0011111", "0101011"
--- new values: [A,A,B,B,A,A,A], [A,B,A,B,A,B,A]
--- returns: 
--- example: verticalPacking [A,A,B,B,A,A,A] [A,B,A,B,A,B,A]
-verticalPacking aTen aEleven = 
-     seekA A aTen
+-- original value(s): "1100111", "1010101", "0011111", "0101011"
+-- new value(s): [A,A,B,B,A,A,A]
+-- returns: [True,True,False,False,True,True,True]
+-- example: verticalPacking A [A,A,B,B,A,A,A]
+verticalPacking dnaChar aTen = 
+     seekChar dnaChar aTen
 
-seekA :: DNA -> [DNA] -> [Bool]
-seekA dnaLetter dnaSequence =
+seekChar :: DNA -> [DNA] -> [Bool]
+seekChar dnaLetter dnaSequence =
     [if x == dnaLetter then True else False | x <- dnaSequence]
+
+-- verticalPackingExpanded A [A,A,B,B,A,A,A] [A,B,A,B,A,B,A]
+-- returns [[True, True], [True, False], [False, True], [False, False], [True, True], [True, False], [True, True]] 
+verticalPackingExpanded dnaLetter aTen aEleven = 
+    zipWith (\x y -> (if x == dnaLetter then True else False) : (if y == dnaLetter then True else False) : []) aTen aEleven
