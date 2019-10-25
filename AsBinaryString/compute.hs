9889d1dd28fbd95bce0a83fcd62a46d5b009d462
javezbak <ja2757@nyu.edu>
2019-10-25 14:53:41 -0400

    parameter values no longer hard coded, empty values now computed

--- AsBinaryString/compute.hs
+++ AsBinaryString/compute.hs
@@ -1,28 +1,17 @@
--- A = 1, B = 0
-data DNA = A | B deriving (Show, Eq)
+-- A = 1, B = 0, Q = ?
+data DNA = A | B | Q deriving (Show, Eq)
 
--- original value(s): "1100111"
--- new value(s): [A,A,B,B,A,A,A]
--- returns: [True,True,False,False,True,True,True]
--- example: verticalPacking A [A,A,B,B,A,A,A]
-verticalPacking dnaChar aTen = 
-     seekChar dnaChar aTen
+-- original value(s): [B,B,A,A,Q,Q,Q] [B,A,B,A,B,A,Q]
+dnaCompute :: [DNA] -> [DNA] -> ([DNA], [DNA])
+dnaCompute cNodeOne cNodeTwo =
+    let aTen = [if x == B || x == Q then A else B | x <- cNodeOne]
+        aEleven = [if x == A || x == Q then A else B | x <- cNodeOne]
+        aTwenty = [if x == B || x == Q then A else B | x <- cNodeTwo]
+        aTwentyOne = [if x == A || x == Q then A else B | x <- cNodeTwo]
 
-seekChar :: DNA -> [DNA] -> [Bool]
-seekChar dnaLetter dnaSequence =
-    [if x == dnaLetter then True else False | x <- dnaSequence]
-
--- verticalPackingExpanded A [A,A,B,B,A,A,A] [A,B,A,B,A,B,A]
--- returns [[True, True], [True, False], [False, True], [False, False], [True, True], [True, False], [True, True]] 
-verticalPackingExpanded dnaLetter aTen aEleven = 
-    zipWith (\x y -> (if x == dnaLetter then True else False) : (if y == dnaLetter then True else False) : []) aTen aEleven
-
--- original value(s): "1100111", "0011111", "1010101", "0101011"
--- new value(s): [A,A,B,B,A,A,A] [B,B,A,A,A,A,A] [A,B,A,B,A,B,A] [B,A,B,A,B,A,A]
-dnaCompute :: [DNA] -> [DNA] ->  [DNA] -> [DNA] -> ([DNA], [DNA])
-dnaCompute aTen aEleven aTwenty aTwentyOne =
-    let rZero = dAnd aTen aTwenty --A10 AND A20
+        rZero = dAnd aTen aTwenty --A10 AND A20
         rOne = dAnd aEleven aTwentyOne --A11 AND A21
+
         xorVal = dXor rZero rOne --R0 XOR R1
         tmp = dFlip xorVal --NOT(R0 XOR R1)
     in (dOr tmp rZero, dOr tmp rOne)
